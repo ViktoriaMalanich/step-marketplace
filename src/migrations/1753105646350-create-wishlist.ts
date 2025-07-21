a354e9e56bef6f69d0ab7eca7f22ad543d53c154
Viktoria Malanych <vmalanich290806@gmail.com>
2025-07-21 21:06:18 +0100

    Create new migration for wishlist

---
+++ src/migrations/1753105646350-create-wishlist.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateWishlist1753105646350 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
